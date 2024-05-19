548922ed0ed4cd3861eda507d7e42b6adac6a760
Aya-moustafa <aiyamoustafa910@gmail.com>
2024-05-19 14:46:25 +0300

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Aya Mostafa on 19/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
